974c7c93891b84f4df4922e02a9bda5a827d082a
Fergal Daly <fergal@chromium.org>
2019-03-18 19:02:25 +0900

    big mess but it compiles

--- src/VirtualContent.mjs
+++ src/VirtualContent.mjs
@@ -1,3 +1,12 @@
+const DEBUG = true;
+
+function DLOG(...messages) {
+  if (!DEBUG) {
+    return;
+  }
+  console.log(...messages);
+}
+
 function composedTreeParent(node) {
   return node.assignedSlot || node.host || node.parentNode;
 }
@@ -44,47 +53,84 @@ const TEMPLATE = `
   contain: layout style
 }
 </style>
-<div id="emptySpaceSentinelContainer"></div>
-<slot></slot>
+<div id="innerContainer">
+  <div id="emptySpaceSentinelContainer"></div>
+  <slot></slot>
+</div>
 `;
 
+class Offset {
+  constructor(offset, element) {
+    this.offset = offset;
+    this.element = element;
+  }
+}
+
+class Range {
+  constructor(low, high, lowElement, highElement) {
+    this.low = low;
+    this.high = high;
+    this.lowElement = lowElement;
+    this.highElement = highElement;
+  }
+
+  // this :  -----------
+  // other:    ------
+  // result: --      ---
+  minus(other) {
+    let lowUncovered, highUncovered;
+    if (this.low < other.low) {
+      lowUncovered = new Range(this.low, other.low, this.lowElement)
+    }
+    if (this.high > other.high) {
+      highUncovered = new Range(other.high, this.high, null, this.highElement)
+    }
+    return [lowUncovered, highUncovered];
+  }
+}
+
 export class VirtualContent extends HTMLElement {
-  #cachedHeights = new WeakMap();
+  #sizes = new WeakMap();
+  #target;
   #toShow = new Set();
   #updateRAFToken;
   #intersectionObserver;
   #mutationObserver;
   #resizeObserver;
+  // The inner container allows us to get the size of the scroller without
+  // forcing layout of the containing doc.
+  #innerContainer;
   #emptySpaceSentinelContainer;
+
+  #innerRect;
+  #unlockedBounds = new Range(0, 0);
+  #toUnlock = new Set();
+
+  #totalMeasuredSize = 0;
+  #measuredCount = 0;
 
   constructor() {
     super();
-
-    this.#intersectionObserverCallback = this.#intersectionObserverCallback.bind(this);
-    this.#mutationObserverCallback = this.#mutationObserverCallback.bind(this);
-    this.#resizeObserverCallback = this.#resizeObserverCallback.bind(this);
-    this.#onActivateinvisible = this.#onActivateinvisible.bind(this);
-    this.#scheduleUpdate = this.#scheduleUpdate.bind(this);
-    this.#update = this.#update.bind(this);
-    this.#showElement = this.#showElement.bind(this);
-    this.#hideElement = this.#hideElement.bind(this);
 
     const shadowRoot = this.attachShadow({mode: 'closed'});
     shadowRoot.innerHTML = TEMPLATE;
     this.#emptySpaceSentinelContainer =
         shadowRoot.getElementById('emptySpaceSentinelContainer');
+    this.#innerContainer =
+        shadowRoot.getElementById('innerContainer');
+    this.#innerRect = this.#innerContainer.getBoundingClientRect()
 
     this.#intersectionObserver =
         new IntersectionObserver(this.#intersectionObserverCallback);
     this.#mutationObserver =
-        new MutationObserver(this.#mutationObserverCallback);
-    this.#resizeObserver = new ResizeObserver(this.#resizeObserverCallback);
+        new MutationObserver(this.mutationObserverCallback);
+    this.#resizeObserver = new ResizeObserver(this.resizeObserverCallback);
     this.#intersectionObserver.observe(this);
 
     // Send a MutationRecord-like object with the current, complete list of
     // child nodes to the MutationObserver callback; these nodes would not
     // otherwise be seen by the observer.
-    this.#mutationObserverCallback([{
+    this.mutationObserverCallback([{
       type: 'childList',
       target: this,
       addedNodes: Array.from(this.childNodes),
@@ -102,35 +148,153 @@ export class VirtualContent extends HTMLElement {
     // invisible="", and thus it will be rendered (allowing us to accurately
     // measure its height, etc.)
     this.addEventListener(
-        'activateinvisible', this.#onActivateinvisible, {capture: true});
+        'activateinvisible', this.onActivateinvisible, {capture: true});
   }
 
-  ["short"](e) {
+  setTarget(offset) {
+    this.targetOffset(offset);
+    this.scheduleUpdate();
+  }
+
+  update() {
+    this.#updateRAFToken = undefined;
+
+    if (target === undefined) {
+      updateToInitial();
+    } else if (target.element) {
+      updateToElement(target);
+    } else {
+      updateToOffset(target.offset);
+    }
+  }
+
+  updateToInitial() {
+    this.displayLock.acquire();
+    try {
+      this.updateToInitialLocked();
+    } finally {
+      this.displayLock.commit();
+    }
+  }
+
+  getBounds(rect) {
+    // TODO: allow horizontal scrolling.
+    return Range(rect.y, rect.height);
+  }
+
+  updateToInitialLocked() {
+    let rect = innerContainer.getBoundingClientRect();
+    let scrollerBounds = this.getBounds(rect);
+    let uncovered = scrollerBounds.minus(this.revealedBounds);
+    for (const bounds of uncovered) {
+      if (bounds) {
+        this.tryRevealBounds(bounds);
+      }
+    }
+  }
+
+  tryRevealBounds(bounds) {
+    if (bounds.lowElement) {
+      this.revealLower(bounds);
+    } else if (bounds.highElement) {
+      this.revealHigher(bounds);
+    } else {
+      this.revealBoth(bounds);
+    }
+  }
+
+  requestReveal(element) {
+    if (!element.displayLock.locked) {
+      DLOG(element, "is already unlocked");
+    }
+    this.#toUnlock.add(element);
+  }
+
+  revealLower(bounds) {
+    revealDirection(bounds, /* lower */ true);
+  }
+
+  revealHigher(bounds) {
+    revealDirection(bounds, /* lower */ false);
+  }
+
+  revealDirection(bounds, lower) {
+    let element = lower ? bounds.lowElement : bounds.highElement;
+    let rect = startElement.getBoundingClientRect();
+    let edge = lower ? rect.y : - (rect.y + rect.height);
+    let limit = lower ? bounds.low : - bounds.high;
+    for (const element of findElements(element, edge, limit, lower)) {
+      requestReveal(element);
+    }
+  }
+
+  findElements(element, edge, limit, lower) {
+    let elements = [];
+    while (edge > limit) {
+      element = lower ? element.previousChild : element.nextChild;
+      elements.push(element);
+      edge -= getSize(element)
+    }
+    return elements;
+  }
+
+  getSize(element) {
+    let size = this.#sizes.get(element);
+    return size === undefined ? getAverageSize() : size;
+  }
+
+  getAverageSize() {
+    return DEFAULT_HEIGHT_ESTIMATE;
+  }
+
+  revealBoth(bounds) {
+    let firstElement = this.firstChild;
+    let elements = findElements(firstElement, 0, bounds.low, /* lower */ false);
+    let startElement = elements ? elements[-1] : firstElement;
+    let elementsToReveal = findElements(startElement, bounds.low, bounds.high, /* lower */ false);
+    for (const element of elementsToReveal) {
+      requestReveal(element);
+    }
+  }
+
+  getScrollerHeight() {
+    const numElements = this.children.length;
+    return this.#totalMeasuredSize + (numElements - this.#measuredCount) * getAverageSize();
+  }
+
+  updateToOffset(offset) {
+  }
+
+  updateToElement(target) {
+  }
+
+  short(e) {
     return e.innerText.substr(0, 3);
   }
 
   #showElement = (e) => {
+    /*
     this.#toShow.add(e);
-    window.requestAnimationFrame(() => {
-      for (const e of this.#toShow) {
-        e.removeAttribute('invisible');
-        e.displayLock.commit();
-        console.log("unlocking", this.short(e));
-      }
-      this.#toShow.clear();
-    });
+    this.#toShow.add(e);
+    this.#scheduleUpdate();
+    */
   }
 
   #hideElement = (e) => {
+    /*
+    this.#toShow.add(e);
+    this.#scheduleUpdate();
     e.setAttribute('invisible', '');
     e.displayLock.acquire({ timeout: Infinity, activatable: true });
     console.log("locking", this.short(e));
     if (this.#toShow.has(e)) {
       this.#toShow.remove(e);
     }
+    */
   }
 
   #intersectionObserverCallback = (entries) => {
+    /*
     for (const {target, isIntersecting} of entries) {
       // Update if the <virtual-content> has moved into or out of the viewport.
       if (target === this) {
@@ -153,34 +317,39 @@ export class VirtualContent extends HTMLElement {
         break;
       }
     }
+    */
   }
 
-  #mutationObserverCallback = (records) => {
-    const estimatedHeights = this.#cachedHeights;
+  #removeElement = (e) => {
+    // Removed children should have be made visible again; they're no
+    // longer under our control.
+    this.#resizeObserver.unobserve(e);
+    this.#intersectionObserver.unobserve(e);
+    this.showElement(e);
+    estimatedHeights.delete(e);
+  }
 
+  #addElement = (e) => {
+    // Added children should be invisible initially. We want to make them
+    // invisible at this MutationObserver timing, so that there is no
+    // frame where the browser is asked to render all of the children
+    // (which could be a lot).
+    // [_update]() will remove invisible="" if it calculates that the
+    // elements could be maybe in the viewport, at which point the
+    // necessary ones will get rendered.
+    this.hideElement(e);
+  }
+
+  mutationObserverCallback(records) {
     for (const record of records) {
       for (const node of record.removedNodes) {
         if (node.nodeType === Node.ELEMENT_NODE) {
-          // Removed children should have be made visible again; they're no
-          // longer under our control.
-          this.#resizeObserver.unobserve(node);
-          this.#intersectionObserver.unobserve(node);
-          this.#showElement(node);
-          estimatedHeights.delete(node);
+          this.#removeElement(node);
         }
       }
 
       for (const node of record.addedNodes) {
         if (node.nodeType === Node.ELEMENT_NODE) {
-          // Added children should be invisible initially. We want to make them
-          // invisible at this MutationObserver timing, so that there is no
-          // frame where the browser is asked to render all of the children
-          // (which could be a lot).
-          // [_update]() will remove invisible="" if it calculates that the
-          // elements could be maybe in the viewport, at which point the
-          // necessary ones will get rendered.
-          this.#hideElement(node);
-          estimatedHeights.set(node, DEFAULT_HEIGHT_ESTIMATE);
         } else {
           // Remove non-element children because we can't control their
           // invisibility state or even prevent them from being rendered using
@@ -195,180 +364,20 @@ export class VirtualContent extends HTMLElement {
       }
     }
 
-    this.#scheduleUpdate();
+//    this.#scheduleUpdate();
   }
 
-  #resizeObserverCallback = () => {
-    this.#scheduleUpdate();
+  resizeObserverCallback() {
   }
 
-  #onActivateinvisible = (e) => {
-    // Find the child containing the target and synchronously update, forcing
-    // that child to be visible. The browser will automatically scroll to that
-    // element because it is visible, which will trigger another update to make
-    // the surrounding nodes visible.
-    let child = e.target;
-    while (child.parentNode !== this) {
-      child = child.parentNode;
-    }
-    this.#update(child);
+  onActivateinvisible(e) {
   }
 
-  #scheduleUpdate = () => {
+  scheduleUpdate() {
     if (this.#updateRAFToken !== undefined)
       return;
 
-    this.#updateRAFToken = window.requestAnimationFrame(this.#update);
+    this.#updateRAFToken = window.requestAnimationFrame(this.update);
   }
 
-  // TODO: this method is enormous. Split it up into several separate steps.
-  // https://refactoring.guru/smells/long-method
-  #update = (childToForceVisible) => {
-    this.#updateRAFToken = undefined;
-
-    const thisClientRect = this.getBoundingClientRect();
-    // Don't read or store layout information if the <virtual-content> isn't in
-    // a renderable state (e.g. disconnected, invisible, `display: none`, etc.).
-    const isRenderable = thisClientRect.top !== 0 ||
-        thisClientRect.left !== 0 || thisClientRect.width !== 0 ||
-        thisClientRect.height !== 0;
-
-    const cachedHeights = this.#cachedHeights;
-    const getAndCacheHeightIfPossible = (child) => {
-      if (isRenderable && !child.hasAttribute('invisible')) {
-        const childClientRect = child.getBoundingClientRect();
-        const style = window.getComputedStyle(child);
-        const height = window.parseFloat(style.marginTop, 10) +
-            window.parseFloat(style.marginBottom, 10) + childClientRect.height;
-        cachedHeights.set(child, height);
-      }
-      return cachedHeights.get(child);
-    };
-
-    const previouslyVisible = new Set();
-    for (let child = this.firstChild; child !== null;
-         child = child.nextSibling) {
-      if (!child.hasAttribute('invisible')) {
-        previouslyVisible.add(child);
-      }
-    }
-
-    let beforePreviouslyVisible = previouslyVisible.size > 0;
-    let nextTop = 0;
-    let renderedHeight = 0;
-
-    // The estimated height of all elements made invisible since the last time
-    // an element was made visible (or start of the child list).
-    let currentInvisibleRunHeight = 0;
-    // The next empty space sentinel that should be reused, if any.
-    let nextEmptySpaceSentinel = this.#emptySpaceSentinelContainer.firstChild;
-    // Inserts an empty space sentinel representing the last contiguous run of
-    // invisible elements. Reuses already existing empty space sentinels, if
-    // possible.
-    const insertEmptySpaceSentinelIfNeeded = () => {
-      if (currentInvisibleRunHeight > 0) {
-        let sentinel = nextEmptySpaceSentinel;
-        if (nextEmptySpaceSentinel === null) {
-          sentinel = document.createElement('div');
-          this.#emptySpaceSentinelContainer.appendChild(sentinel);
-        }
-        nextEmptySpaceSentinel = sentinel.nextSibling;
-
-        const sentinelStyle = sentinel.style;
-        console.log(`sentinel top ${nextTop - currentInvisibleRunHeight}`);
-        sentinelStyle.top = `${nextTop - currentInvisibleRunHeight}px`;
-        console.log(`sentinel height ${currentInvisibleRunHeight}px`);
-        sentinelStyle.height = `${currentInvisibleRunHeight}px`,
-
-        this.#intersectionObserver.observe(sentinel);
-
-        currentInvisibleRunHeight = 0;
-      }
-    };
-
-    for (let child = this.firstChild; child !== null;
-         child = child.nextSibling) {
-      if (beforePreviouslyVisible && previouslyVisible.has(child)) {
-        beforePreviouslyVisible = false;
-      }
-
-      // At this point the element might not be rendered, so this either gets
-      // the current height (if rendered) or the last known, possibly
-      // inaccurate, height.
-      let possiblyCachedHeight = getAndCacheHeightIfPossible(child);
-
-      const childClientTop = thisClientRect.top + nextTop;
-
-      // This is based on the height above, so it might not be correct.
-      // If it turns out to be true, then we make the element visible and read
-      // its height more exactly.
-      const maybeInViewport = (0 <= childClientTop + possiblyCachedHeight) &&
-          (childClientTop <= window.innerHeight);
-
-      if (maybeInViewport || child === childToForceVisible) {
-        if (child.hasAttribute('invisible')) {
-          this.#showElement(child);
-          this.#resizeObserver.observe(child);
-          this.#intersectionObserver.observe(child);
-
-          // Since we just flipped to be visible, we should recalculate the
-          // height and update the cache.
-          const previousCachedHeight = possiblyCachedHeight;
-          possiblyCachedHeight = getAndCacheHeightIfPossible(child);
-
-          if (beforePreviouslyVisible) {
-            const scrollingAncestor = nearestScrollingAncestor(this);
-            if (scrollingAncestor !== null) {
-              scrollingAncestor.scrollBy(
-                  0, possiblyCachedHeight - previousCachedHeight);
-            }
-          }
-        }
-
-        // At this point possiblyCachedHeight is exact, so we can use the same
-        // technique as we did when calculating maybeInViewport, but this time
-        // we will have a guaranteed-correct answer.
-        const isInViewport = (0 <= childClientTop + possiblyCachedHeight) &&
-            (childClientTop <= window.innerHeight);
-
-        console.log(`nextTop ${nextTop}`, this.short(child));
-        if (isInViewport || child === childToForceVisible) {
-          insertEmptySpaceSentinelIfNeeded();
-
-          console.log(`setting top ${nextTop}`, this.short(child));
-          child.style.top = `${nextTop - renderedHeight}px`;
-          renderedHeight += possiblyCachedHeight;
-        } else {
-          this.#hideElement(child);
-          this.#resizeObserver.unobserve(child);
-          this.#intersectionObserver.unobserve(child);
-
-          currentInvisibleRunHeight += possiblyCachedHeight;
-        }
-      } else {
-        if (!child.hasAttribute('invisible')) {
-          this.#hideElement(child);
-          this.#resizeObserver.unobserve(child);
-          this.#intersectionObserver.unobserve(child);
-        }
-
-        currentInvisibleRunHeight += possiblyCachedHeight;
-      }
-
-      nextTop += possiblyCachedHeight;
-    }
-
-    insertEmptySpaceSentinelIfNeeded();
-
-    // Remove any extra empty space sentinels.
-    while (nextEmptySpaceSentinel !== null) {
-      const sentinel = nextEmptySpaceSentinel;
-      nextEmptySpaceSentinel = sentinel.nextSibling;
-
-      this.#intersectionObserver.unobserve(sentinel);
-      sentinel.remove();
-    }
-
-    this.style.height = `${nextTop}px`;
-  }
 }
