6e19b8a7caabfd45c8b74fd19101a526e61e0f0f
Fergal Daly <fergal@esatclear.ie>
2019-06-27 10:38:04 +0900

    move findElement stuff to FindElement.mjs

--- src/VirtualContent.mjs
+++ src/VirtualContent.mjs
@@ -1,4 +1,5 @@
 import * as Sets from './Sets.mjs';
+import * as FindElement from './FindElement.mjs';
 
 
 const BUFFER = 0.2;
@@ -45,47 +46,9 @@ class ElementBounds {
   }
 }
 
-const BIAS_LOW = Symbol("BIAS_LOW");
-const BIAS_HIGH = Symbol("BIAS_HIGH");
-// Binary searches inside the list |elements| to find which element's
-// vertical bounds contain |offset|.  Assumes that the elements are
-// already sorted in increasing pixel order.  |bias| controls what
-// happens if |offset| is not contained within any element.  If |bias|
-// is BIAS_LOW, then this selects the lower element nearest |offset|,
-// otherwise it selects the higher element.
-function findElementIndex(elements, offset, bias) {
-  let low = 0;
-  let high = elements.length - 1;
-  let [high_dec, low_inc] = bias === BIAS_LOW ? [1, 0] : [0, 1];
-  let i;
-  while (true) {
-    if (low === high) {
-      return low;
-    }
-    i = Math.floor((low + high) / 2); // eslint-disable-line no-magic-numbers
-    const element = elements[i];
-    const rect = element.getBoundingClientRect();
-    if (rect.top > offset) {
-      // The entire rect is > offset.
-      high = Math.max(i - high_dec, low);
-    } else if (rect.bottom < offset) {
-      // The entire rect is < offset.
-      low = Math.min(i + low_inc, high);
-    } else {
-      // The rect contains offset.
-      break;
-    }
-  }
-  return i;
-}
-
-function findElement(elements, offset) {
-  return elements[findElementIndex(elements, offset)];
-}
-
 function findElementBounds(elements, low, high) {
-  const lowElement = findElement(elements, low, BIAS_LOW);
-  const highElement = findElement(elements, high, BIAS_HIGH);
+  const lowElement = FindElement.findElement(elements, low, FindElement.BIAS_LOW);
+  const highElement = FindElement.findElement(elements, high, FindElement.BIAS_HIGH);
 
   return new ElementBounds(lowElement, highElement);
 }
