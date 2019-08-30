386c3b1a45b59fc9f2744073ad2c137ca9a7bd9c
Fergal Daly <fergal@esatclear.ie>
2019-08-30 15:03:38 +0900

    Add gh-pages revision 2347b5d9856476de1cee26d35f5f2044de853794

---
+++ versions/2347b5d9856476de1cee26d35f5f2044de853794/demo/jank/RevealTree.mjs
@@ -0,0 +1,179 @@
+'use strict';
+const DEBUG = 0;
+
+import * as Locker from '../util/Locker.mjs';
+
+const TEMPLATE = `
+<style>
+div {
+  display: block !important;
+  contain: layout style;
+}
+
+div.revealed {
+  display: contents;
+  contain:;
+}
+
+slot.revealed {
+  display: contents;
+}
+</style>
+`;
+
+class RevealTree extends HTMLElement {
+  root;
+  branch;
+  revealed = new Set();
+  childToSlot = new WeakMap();
+
+  constructor() {
+    super();
+  }
+
+  initShadowRoot() {
+    const options = {mode: 'closed'};
+    if (this.useISA) {
+      options["slotting"] = "manual";
+    }
+    const shadowRoot = this.attachShadow(options);
+    shadowRoot.innerHTML = TEMPLATE;
+    this.root = document.createElement("div");
+    this.root.id = "root";
+    shadowRoot.appendChild(this.root);
+  }
+
+  populate() {
+    this.slotPerChild = parseInt(this.getAttribute("slot-per-child"));
+    this.branch = parseInt(this.getAttribute("branch")) || 10;
+    this.useISA = parseInt(this.getAttribute("use-isa")) || 0;
+
+    this.initShadowRoot();
+
+    let slots = [];
+
+    for (const child of this.children) {
+      const slot = document.createElement("slot");
+      slots.push(slot);
+      this.assign(slot, child, slots.length);
+    }
+
+    this.slots = slots;
+    let tree = this.createTree(slots);
+    this.tree = tree;
+    if (tree) {
+      this.root.appendChild(tree[0]);
+      this.labelTree(tree[0], "d0");
+    }
+  }
+
+  labelTree(tree, label) {
+    if (tree == null) {
+      return;
+    }
+    tree.id = label;
+    for (let i = 0; i < tree.children.length; i++) {
+      this.labelTree(tree.children[i], label + i);
+    }
+  }
+
+  assign(slot, element, name) {
+    this.childToSlot.set(element, slot);
+    if (this.useISA) {
+      slot.assign([element]);
+    } else {
+      element.slot = name;
+      slot.name = name;
+    }
+  }
+
+  createTree(slots) {
+    if (slots.length == 0) {
+      return null;
+    }
+
+    let divLayer = this.createLeafDivLayer(slots);
+    while (true) {
+      divLayer = this.createDivLayer(divLayer);
+      if (divLayer.length == 1) {
+        return divLayer;
+      }
+    }
+  }
+
+  createLeafDivLayer(slots) {
+    let divs = [];
+    for (const slot of slots) {
+      let div = document.createElement("div");
+      div.lockSize = 50;
+      divs.push(div);
+      div.appendChild(slot);
+      Locker.locker.lock(div);
+    }
+    return divs;
+  }
+
+  createDivLayer(divs) {
+    let newDivs = [];
+    for (let i = 0; i < divs.length; i++) {
+      let div = document.createElement("div");
+      div.lockSize = 0;
+      newDivs.push(div);
+      for (let j = 0; j < this.branch && i < divs.length; j++) {
+        div.appendChild(divs[i]);
+        div.lockSize += divs[i].lockSize;
+        i++;
+      }
+      Locker.locker.lock(div);
+      i--;
+    }
+    return newDivs;
+  }
+
+  findAncestorsForElements(elements, ancestors) {
+    for (const element of elements) {
+      this.findAncestorsForSlot(this.childToSlot.get(element), ancestors);
+    }
+  }
+
+  findAncestorsForSlot(slot, ancestors) {
+    let element = slot.parentElement;
+    if (!element) {
+      return;
+    }
+    while (element != this.root && !ancestors.has(element)) {
+      ancestors.add(element);
+      element = element.parentElement;
+    }
+  }
+
+  revealElements(elements) {
+    let ancestors = new Set();
+    this.findAncestorsForElements(elements, ancestors);
+    this.updateRevealed(ancestors);
+  }
+
+  updateRevealed(newRevealed) {
+    const revealed = [];
+    for (const e of this.revealed) {
+      if (!newRevealed.has(e)) {
+        e.className = "";
+        Locker.locker.lock(e);
+        revealed.push(e);
+      }
+    }
+    if (DEBUG) console.log("revealed", revealed);
+    const hidden = [];
+    for (const e of newRevealed) {
+      if (!this.revealed.has(e)) {
+        e.className = "revealed";
+        Locker.locker.unlock(e);
+      }
+      hidden.push(e);
+    }
+    if (DEBUG) console.log("hidden", hidden);
+    this.revealed = newRevealed;
+  }
+}
+
+customElements.define('reveal-tree', RevealTree);
