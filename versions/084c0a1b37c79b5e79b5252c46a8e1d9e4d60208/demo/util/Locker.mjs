70d3a8ebe5ce14a6ee552e7677b205482e3c7713
Fergal Daly <fergal@esatclear.ie>
2019-08-30 16:57:52 +0900

    Add gh-pages revision 084c0a1b37c79b5e79b5252c46a8e1d9e4d60208

---
+++ versions/084c0a1b37c79b5e79b5252c46a8e1d9e4d60208/demo/util/Locker.mjs
@@ -0,0 +1,32 @@
+export class Locker {
+  constructor(size) {
+    this.size = size || [10, 50];
+  }
+
+  lock(element, andThen) {
+    let size = element.lockSize ? [10, element.lockSize] : this.size;
+    return element.displayLock.acquire({
+      timeout: Infinity,
+      activatable: true,
+      size: size,
+    }).then(andThen, reason => {console.log("Rejected: ", reason.message)});
+  }
+
+  update(element, andThen) {
+    return element.displayLock.update().then(andThen, reason => {console.log("Rejected: ", reason.message)});
+  }
+
+  unlock(element, andThen) {
+    return element.displayLock.commit().then(andThen, reason => {console.log("Rejected: ", reason)});
+  }
+
+  warn(element) {
+    console.warn("Display locking not available");
+    let div = document.createElement("div");
+    div.style.color = "red";
+    div.innerText = "No display locking";
+    element.insertBefore(div, element.firstElementChild);
+  }
+}
+
+export const locker = new Locker();
