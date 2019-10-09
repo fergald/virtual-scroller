151d36269559ab3a68bc952221b9deaff1cd113d
Fergal Daly <fergal@esatclear.ie>
2019-10-09 10:53:45 +0900

    Add gh-pages revision 7e95e0c9b8c1ea39ed0b8e9368b4af57eca83b3e

---
+++ versions/7e95e0c9b8c1ea39ed0b8e9368b4af57eca83b3e/demo/util/confirm-features.mjs
@@ -0,0 +1,31 @@
+function redP(textContent) {
+  const p = document.createElement('p');
+  p.style.color = 'red';
+  p.textContent = textContent;
+  return p;
+}
+
+/**
+ * Checks that the features needed are present in the browser. If not,
+ * it places error messages inside |element|.
+ **/
+class ConfirmFeatures extends HTMLElement {
+  constructor() {
+    super();
+    if (!('contentSize' in element.style) {
+      this.appendChild(redP('CSSContentSize is not available'));
+    }
+
+    if (!('renderSubtree' in element)) {
+      this.appendChild(redP('rendersubtree is not available'));
+    }
+
+    const slot = document.createElement('slot');
+    if (!slot.assign) {
+      const div = redP('Manual Slot Assignment is not available');
+      this.appendChild(div);
+    }
+  }
+}
+
+customElements.define('confirm-features', ConfirmFeatures);
