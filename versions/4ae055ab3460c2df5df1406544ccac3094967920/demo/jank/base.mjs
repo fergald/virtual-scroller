613a6596e689b82f79340e62f8f2d756fd283e56
Fergal Daly <fergal@esatclear.ie>
2020-10-24 01:11:47 +0900

    Add gh-pages revision 4ae055ab3460c2df5df1406544ccac3094967920

---
+++ versions/4ae055ab3460c2df5df1406544ccac3094967920/demo/jank/base.mjs
@@ -0,0 +1,82 @@
+import * as Words from '../util/Words.mjs';
+import * as Locker from '../util/Locker.mjs'
+import * as Params from '../util/Params.mjs'
+
+export function populate(vc, statusDiv, content) {
+  if (vc.setFromUrl) {
+    vc.setFromUrl(document.location, statusDiv);
+  } else {
+    console.warn("Not calling vs.setFromUrl on", vc);
+  }
+
+  let wordCount = Params.get("words", (p) => { return parseInt(p) || 50 }, statusDiv);
+  if (!content) {
+    let divCount = Params.get("divs", (p) => { return parseInt(p) || 10000 }, statusDiv);
+    let words = Words.words(wordCount);
+    const f = new DocumentFragment();
+    for (const e of Words.divs(divCount, words)) {
+      f.append(e);
+    }
+    content = f.children;
+  }
+  let i = 0;
+  // Will be NaN if not supplied and i>=lockFrom will never be true.
+  let lockFrom = Params.get("lockFrom", parseInt, statusDiv);
+  while(content.length) {
+    vc.appendChild(content[0]);
+    if (i >= lockFrom) {
+      div.displayLock.acquire({
+        timeout: Infinity,
+        activatable: true,
+        size: [10, 10],
+      }).then(null, reason => {console.log("Rejected: ", reason.message)});
+    }
+  }
+}
+
+function jump() {
+  let elements = contentDiv.children[0].children;
+  elements[elements.length - 1].scrollIntoView();
+}
+window.jump = jump;
+
+function resize() {
+  contentDiv.style.width = contentDiv.style.width == "200px" ? "" : "200px";
+}
+window.resize = resize;
+
+function logInfo() {
+  vc.logInfo();
+}
+window.logInfo = logInfo;
+
+function onChangeUseScrollEvents() {
+  if (vc) {
+    vc.setUseScrollEvents(useScrollEvents.checked, contentDiv);
+  }
+}
+window.onChangeUseScrollEvents = onChangeUseScrollEvents;
+
+function onChangeUseForcedLayouts() {
+  if (vc) {
+    vc.setUseScrollEvents(useForcedLayouts.checked, contentDiv);
+  }
+}
+window.onChangeUseForcedLayouts = onChangeUseForcedLayouts;
+
+export function everyNFrames(n, callback) {
+  let i = 0;
+  function update() {
+    if ((i % n) == 0) {
+      callback(i);
+    }
+    schedule();
+    i++;
+  }
+  function schedule() {
+    window.requestAnimationFrame(
+      update
+    );
+  }
+  schedule();
+}
